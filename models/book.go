1b8cc6e8567ab66e3bdc52330b113b4ff22d5381
husnulnawafil <husnulnawafil27@gmail.com>
2022-12-29 01:55:31 +0700

    add user & book model

--- models/book.go
+++ models/book.go
@@ -1,4 +1,14 @@
 package models
 
+import "gorm.io/gorm"
+
 type Book struct {
+	gorm.Model
+	Owner     int
+	Name      string
+	Author    string
+	Publisher string
+	ISBN      string
+	Price     string
+	Stock     int
 }
--- models/user.go
+++ models/user.go
@@ -1,4 +1,12 @@
 package models
 
+import "gorm.io/gorm"
+
 type User struct {
+	gorm.Model
+	FirstName string `gorm:"not null" json:"first_name" form:"first_name"`
+	Email     string `gorm:"not null" json:"email" form:"email"`
+	Phone     string `gorm:"not null" json:"phone" form:"phone"`
+	LastName  string `gorm:"not null" json:"last_name" form:"last_name"`
+	Balance   int    `gorm:"not null" json:"balance" form:"balance"`
 }
